9696ab8e7b85130b0a4ef6beb27ca59aa82e9d64
Tsvetoslav Tsvetkov <tsvetoslav.tsvetkov@gmail.com>
2022-12-21 14:19:11 +0200

    Remove white spaces program

---
+++ src/main/java/org/hdlservices/RemoveWhiteSpaces.java
@@ -0,0 +1,29 @@
+package org.hdlservices;
+
+import java.util.Scanner;
+
+public class RemoveWhiteSpaces {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter input string to be cleaned from white spaces...!");
+        String inputString = sc.nextLine();
+        System.out.println("Input String : " + inputString);
+
+        // Remove spaces Built-in method
+        String stringWithoutSpaces = inputString.replaceAll("\\s+", "");
+        System.out.println("Input String Without Spaces Built-in: " + stringWithoutSpaces);
+
+        // Remove spaces custom
+        char[] charArray = inputString.toCharArray();
+        StringBuffer stringBufferWithoutSpaces = new StringBuffer();
+        for (int i = 0; i < charArray.length; i++) {
+            if ( (charArray[i] != ' ') && (charArray[i] != '\t') )
+            {
+                stringBufferWithoutSpaces.append(charArray[i]);
+            }
+        }
+        System.out.println("Input String Without Spaces custom: "+stringBufferWithoutSpaces.toString());
+
+        sc.close();
+    }
+}
