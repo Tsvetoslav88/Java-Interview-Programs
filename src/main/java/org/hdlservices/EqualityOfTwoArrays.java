4f344c7ca8f63b0b8345243c8e5d826049018b56
Tsvetoslav Tsvetkov <tsvetoslav.tsvetkov@gmail.com>
2023-01-03 18:46:39 +0200

    Check if two arrays are equal

---
+++ src/main/java/org/hdlservices/EqualityOfTwoArrays.java
@@ -0,0 +1,41 @@
+package org.hdlservices;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class EqualityOfTwoArrays {
+    public static void main(String[] args) {
+        String[] s1 = {"java", "swings", "j2ee", "struts", "jsp", "hibernate"};
+
+        String[] s2 = {"java", "struts", "j2ee", "hibernate", "swings", "jsp"};
+
+        // Sort both arrays
+        Arrays.sort(s1);
+        Arrays.sort(s2);
+
+        // 1) Compare using Iterative Method
+        boolean areEqual = true;
+        if (s1.length == s2.length) {
+            for (int i = 0; i < s1.length; i++) {
+                if (!s1[i].equals(s2[i])) {
+                    areEqual = false;
+                }
+            }
+        } else {
+            areEqual = false;
+        }
+
+        if (areEqual) {
+            System.out.println("1. Two Arrays Are Equal");
+        } else {
+            System.out.println("1. Two Arrays Are Not Equal");
+        }
+
+        // 2) Using Arrays.equals() Method
+        if (Arrays.equals(s1, s2)) {
+            System.out.println("2. Two Arrays Are Equal");
+        } else {
+            System.out.println("2. Two Arrays Are Not Equal");
+        }
+    }
+}
